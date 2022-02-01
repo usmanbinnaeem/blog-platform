16f4a3faecf946f6feadbc603f5590b5ddfdbb64
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-02-02 00:39:38 +0500

    added toast

---
+++ pages/enter.js
@@ -0,0 +1,19 @@
+export default function Enter(props) {
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+function SignInButton(){
+
+}
+
+function SignOutButton(){
+
+}
+
+function UserNameForm(){
+
+}
--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const signUp = () => {
   return (
     <div>
